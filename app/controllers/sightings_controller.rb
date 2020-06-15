043245cb10ba5a2d77256b33e7fdcf62b8b9095e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-15 02:39:39 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,13 +1,14 @@
 class SightingsController < ApplicationController
   
+  def index
+    sightings = Sighting.all
+    render json: sightings.to_json(include: [:bird, :location])
+  end
+  
   def show
     sighting = Sighting.find_by(id: params[:id])
     render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
   end
   
-  def index
-    sightings = Sighting.all
-    render json: sightings, include: [:bird, :location]
-  end
   
 end
